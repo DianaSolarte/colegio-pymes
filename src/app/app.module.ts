97288eeca4443adfe722c14a0932d31ec74d0e25
DianaSolarte <dianasolartem@gmail.com>
2023-01-12 02:24:11 -0500

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { Slider1Component } from './slider1/slider1.component';
+import { CartComponent } from './cart/cart.component';
+import { DocumentComponent } from './document/document.component';
+import { DocumentListComponent } from './document-list/document-list.component';
+import { MedalComponent } from './medal/medal.component';
+import { ProgramsComponent } from './programs/programs.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { CampusComponent } from './campus/campus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    NavComponent,
+    FooterComponent,
+    Slider1Component,
+    CartComponent,
+    DocumentComponent,
+    DocumentListComponent,
+    MedalComponent,
+    ProgramsComponent,
+    ScheduleComponent,
+    CampusComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/medal/medal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medal',
+  templateUrl: './medal.component.html',
+  styleUrls: ['./medal.component.scss']
+})
+export class MedalComponent {
+
+}
